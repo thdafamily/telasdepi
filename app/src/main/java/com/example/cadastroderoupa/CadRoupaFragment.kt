70b950356fd3744222ea0ab50d3fe4c3d9ac8479
thdafamily <thiagovieirasilva35@gmail.com>
2025-09-01 21:21:18 -0300

    oi

---
+++ app/src/main/java/com/example/cadastroderoupa/CadRoupaFragment.kt
@@ -0,0 +1,20 @@
+package com.example.cadastroderoupa
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CadRoupaFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_cad_roupa, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
