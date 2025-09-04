4aa15e41f766be00d6fdf454dd13be4dd0b6d8b0
20231tiimi0227 <thiagovieirasilva35@gmail.com>
2025-09-04 11:01:18 -0300

    oi

---
+++ app/src/main/java/com/example/cadastroderoupa/Extensions.kt
@@ -0,0 +1,12 @@
+package com.example.cadastroderoupa
+
+import androidx.fragment.app.Fragment
+
+fun Fragment.showBottomSheet(
+    titleDialog: Int? = null,
+    titleButtom: Int? = null,
+    message: Int,
+    onClick: () -> Unit ={}
+){
+
+}
